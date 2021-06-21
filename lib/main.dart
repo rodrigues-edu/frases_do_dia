e30bf63e869ad8330193a1f5449627743f7a33c3
Eduardo Rodrigues da Silva <silva.rodriguesedu@icloud.com>
2021-06-21 08:45:47 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Frases do Dia"),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          //width: double.infinity,
+          /*decoration: BoxDecoration(
+            border: Border.all(width: 3, color: Colors.amber),
+          ),*/
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset("images/logo.png"),
+              Text(
+                "Clique abaixo para gerar uma frase!",
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  fontSize: 25,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black,
+                ),
+              ),
+              RaisedButton(
+                child: Text(
+                  "Nova Frase",
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                color: Colors.green,
+                onPressed: (){},
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
